ce18e88e0250ce79f5c2e305801eec19c98dc1d2
arusmanukyan <manukyan.arus@gmail.com>
2017-01-20 18:18:53 -0800

    server

--- server.js
+++ server.js
@@ -14,3 +14,17 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+var exphbs = require("express-handlebars");
+app.engine("handlebars", exphbs({ defaultlayout: "main"}));
+app.set("view engine", "handlebars");
+
+var connection = require("./config/connection.js");
+
+var routes = require("./controllers/burger_controller.js");
+
+app.use("/", routes);
+
+app.listen(port, function(){
+	console.log("Listening to port " + port);
+});
