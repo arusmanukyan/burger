8d75368fa3fc64bc3b1813c7048c089736371891
arusmanukyan <manukyan.arus@gmail.com>
2017-01-20 18:00:46 -0800

    controller

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -5,5 +5,36 @@ var router = expreess.Router();
 var burger = require("../models/burger.js");
 
 router.get("/", function(req, res){
-	
-})
+	res.redirect("/burgers");
+});
+
+router.get("/burgers", function(req, res){
+	burger.selectAll(function(data){
+		var hbsObject = {
+			burgers: data
+		};
+		console.log(hbsObject);
+		res.render("index", hbsOject);
+	});
+});
+
+router.post("/burgers/update/:id", function(req, res){
+	burger.create([
+		"name", "devoured"
+		],[
+			res.redirect("/burgers");
+		]);
+});
+
+router.put("/burger/update/:id", function(req, res){
+	var condition = "id = " + req.params.id;
+	console.log("condition", condition);
+
+	burgers.update({
+		devoured: req.body.devoured
+	}, condition, function(){
+		res.redirect("/burgers");
+	});
+});
+
+module.exports = router;
---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+
+INSERT INTO burgers (burger_name) VALUES ('Double Double');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie Burger', false);
